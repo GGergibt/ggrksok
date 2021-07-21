b338f832b4ef08f5e89abfabe5c0c423abef709f
GGerbit <gosha2007naz@gmail.com>
2021-07-21 21:33:39 +0300

    реализовал проверку запроса ЗОПИШИ

--- python_socket_server.py
+++ python_socket_server.py
@@ -1,13 +1,29 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("localhost", 50000))
-s.listen(1)
-conn, addr = s.accept()
-while 1:
-    data = conn.recv(1024)
-    if not data:
+
+server = socket.create_server(("localhost", 5000))
+server.listen(1)
+conn, addr = server.accept()
+while True:
+    name = conn.recv(1024)
+    my = name.decode().split()
+    if my[0] == "ЗОПИШИ":
+    #response = my[0]
+         if len(my) == 5: 
+             name = (my[1], my[2])
+             name = "".join(name)
+             conn.sendall("НОРМАЛДЫКС РКСОК/1.0".encode())
+             conn.sendall(my)
+             print(my)
+         elif len(my) == 3:
+             name = my[1]
+             print(name)
+         else:
+             print("not understand")
+
+
+
+    if not my:
         break
-    conn.sendall(data)
-    print(data.decode())
-conn.close()
+    #conn.sendall(my)
+server.close()
