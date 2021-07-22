80e6f98ce6d805e0e64df81d3cbbcfdf72f7f712
GGerbit <gosha2007naz@gmail.com>
2021-07-22 17:57:14 +0300

    merge branch

--- python_socket_server.py
+++ python_socket_server.py
@@ -2,28 +2,30 @@ import socket
 
 
 server = socket.create_server(("localhost", 5000))
-server.listen(1)
-conn, addr = server.accept()
+server.listen(2)
+
+#def checking_len_of_request():
+
 while True:
-    name = conn.recv(1024)
-    my = name.decode().split()
-    if my[0] == "ЗОПИШИ":
-    #response = my[0]
-         if len(my) == 5: 
-             name = (my[1], my[2])
-             name = "".join(name)
-             conn.sendall("НОРМАЛДЫКС РКСОК/1.0".encode())
-             conn.sendall(my)
-             print(my)
-         elif len(my) == 3:
-             name = my[1]
-             print(name)
-         else:
-             print("not understand")
-
-
-
-    if not my:
+    print("we are on the top level")
+    conn, addr = server.accept()
+    while True:
+        name = conn.recv(1024)
+        print(name)
+        # my = name.decode().split()
+        # if my[0] == "ЗОПИШИ":
+        #      if len(my) == 5:
+        # name = (my[1], my[2])
+        # name = "".join(name)
+        conn.sendall("НОРМАЛДЫКС РКСОК/1.0".encode())
+        conn.close()
         break
-    #conn.sendall(my)
+             # elif len(my) == 3:
+             #     name = my[1]
+             #     print(name)
+             # else:
+             #    pass
+
+
+        #conn.sendall(my)
 server.close()
