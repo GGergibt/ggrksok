1401e5eada7647ccf4a0f4f385084121e3856950
d1mnaz <dimonnaz@gmail.com>
2021-07-22 17:47:25 +0300

    настрока отправки сообщения

--- python_socket_server.py
+++ python_socket_server.py
@@ -1,29 +1,19 @@
 import socket
 
+s = socket.create_server(("localhost", 5000))
+s.listen(1)
 
-server = socket.create_server(("localhost", 5000))
-server.listen(1)
-conn, addr = server.accept()
 while True:
-    name = conn.recv(1024)
-    my = name.decode().split()
-    if my[0] == "ЗОПИШИ":
-    #response = my[0]
-         if len(my) == 5: 
-             name = (my[1], my[2])
-             name = "".join(name)
-             conn.sendall("НОРМАЛДЫКС РКСОК/1.0".encode())
-             conn.sendall(my)
-             print(my)
-         elif len(my) == 3:
-             name = my[1]
-             print(name)
-         else:
-             print("not understand")
-
-
-
-    if not my:
+    print("Главный цикл")
+    conn, addr = s.accept()
+    while True:
+        data = conn.recv(1024)
+        # if not data:
+        #     break
+        res = "НОРМАЛДЫКС РКСОК/3.0".encode()
+        print(data.decode())
+        # conn.send(res)
+        conn.sendall(res)
+        conn.close()
         break
-    #conn.sendall(my)
-server.close()
+s.close()
