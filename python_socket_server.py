9e2c9e78d1f6dc2abc857b3367f5aca1ff77689a
d1mnaz <dimonnaz@gmail.com>
2021-07-27 00:51:59 +0300

    Перенёс функции в класс. Добавлен gitignore

--- python_socket_server.py
+++ python_socket_server.py
@@ -9,80 +9,98 @@ NOTFOUND = "НИНАШОЛ"
 NOT_APPROVED = "НИЛЬЗЯ"
 INCORRECT_REQUEST = "НИПОНЯЛ"
 
-name = ""
-phone_response = ""
 
+class RKSOKPhoneBook:
+    """Phonebook working with RKSOK server."""
 
-def send_to_checking_server(res: str) -> bool:
-    """Функция для запроса на сервер проверки"""
-    method = "АМОЖНА? РКСОК/1.0"
-    conn = socket.create_connection(("vragi-vezde.to.digital", 51624))
-    request = f"{method}\r\n {res}\r\n\r\n".encode()
-    conn.send(request)
-    res_vragi = conn.recv(1024).decode()
-    permit = parse_response_check_server(res_vragi)
-    return permit, res_vragi
+    def __init__(self, conn: str):
+        self._conn = conn
+        self._name, self._phone, self._verb, self._method = None, None, None, None
 
+    def get_request(self):
+        """Функция для обработки запроса от клиента"""
+        raw_request = self._conn.recv(1024).decode()
+        body_request = [x for x in raw_request.split("\r\n") if len(x) > 0]
+        response_client = self.compile_response(body_request)
+        return self._conn.send(response_client)
 
-def parse_response_check_server(res: str) -> bool:
-    """Функция для проверки МОЖНО или НИЛЬЗЯ"""
-    if res.startswith("МОЖНА"):
-        return True
-    else:
-        return False
-
-
-def send_response(conn: str, response: str):
-    """Функция для отправки ответа клиенту"""
-    conn.sendall(response)
-
-
-def checking_len_of_name(name: str) -> bool:
-    """Функция проверки длины имени пользователя"""
-    return len(name) <= 30
-
-
-def get_request(conn: str) -> str:
-    """Функция для обработки запроса от клиента"""
-    global phone_response
-    raw_request = conn.recv(1024).decode()
-    body_request = [x for x in raw_request.split("\r\n") if len(x) > 0]
-    if len(body_request) > 1:
-        body_response, name = parse_request(body_request[0])
-        phone_response = parse_phone(body_request[-1])
-        response = f"{body_response}\r\n{phone_response}"
-        return response
-    else:
-        response = parse_request(body_request[0])
-        return response
-
-
-def parse_request(req: str) -> str:
-    """Функция для обработки запроса и получения имени пользователя и если есть телефона"""
-    if req.startswith(tuple(METOD)):
-        global name
-        verbs = req.split()
-        metod = verbs[0]
-        verbs.remove("РКСОК/1.0")
-        verbs.pop(0)
-        name = " ".join(verbs)
-        if checking_len_of_name(name):
-            body_response = f"{metod} {name} {PROTOCOL}"
-            return body_response, name
-
+    def compile_response(self, res):
+        """Функция собирает ответ для клиента"""
+        if len(res) > 1:
+            body_response = self.parse_body_request(res[0])
+            self.parse_phone_request(res[-1])
+            response = f"{body_response}\r\n{self._phone}"
         else:
-            response = f"{INCORRECT_REQUEST} {PROTOCOL}"
-            return response
+            response = self.parse_body_request(res[0])
 
+        response_client = self.send_to_checking_server(response)
+        return response_client
 
-def parse_phone(phone_req: str) -> str:
-    """Функция для обработки номера телефона"""
-    phone = "".join(filter(str.isdigit, phone_req))
-    if len(phone) == 11 and (phone.startswith("89") or phone.startswith("79")):
-        phone = phone[1::]
-    if len(phone) == 10 and phone.startswith("9"):
-        phone = f"8 ({phone[0:3]}) {phone[3:6]}-{phone[6:8]}-{phone[8::]}"
-    return phone
+    def parse_body_request(self, req: str) -> str:
+        """Функция для обработки запроса и получения имени пользователя и если есть телефона"""
+        if req.startswith(tuple(METOD)):
+            verbs = req.split()
+            self._method = verbs[0]
+            verbs.remove(PROTOCOL)
+            verbs.pop(0)
+            self._name = " ".join(verbs)
+            if self.checking_len_of_name(self._name):
+                raw_response = f"{self._method} {self._name} {PROTOCOL}"
+            else:
+                raw_response = f"{INCORRECT_REQUEST} {PROTOCOL}"
+            return raw_response
+
+    def parse_phone_request(self, phone_req: str) -> str:
+        """Функция для обработки номера телефона"""
+        self._phone = "".join(filter(str.isdigit, phone_req))
+        return self._phone
+
+    def checking_len_of_name(self, name: str) -> bool:
+        """Функция проверки длины имени пользователя"""
+        return len(name) <= 30
+
+    def send_to_checking_server(self, res: str) -> str:
+        """Функция для запроса на сервер проверки"""
+        method = "АМОЖНА? РКСОК/1.0"
+        print(res)
+        conn = socket.create_connection(("vragi-vezde.to.digital", 51624))
+        request = f"{method}\r\n {res}\r\n\r\n".encode()
+        conn.send(request)
+        res_vragi = conn.recv(1024).decode()
+        if self.parse_response_check_server(res_vragi):
+            if self._method == "ОТДОВАЙ":
+                path = Path(f"phonebook/{self._name}.txt")
+                if path.exists():
+                    with path.open() as f:
+                        phone = "".join(f.readlines())
+                    response = f"{OK} {PROTOCOL}\r\n{phone}\r\n\r\n".encode()
+                else:
+                    response = f"{NOTFOUND} {PROTOCOL}\r\n\r\n".encode()
+                return response
+            elif self._method == "ЗОПИШИ":
+                with open(f"phonebook/{self._name}.txt", "w+") as file:
+                    file.write(self._phone)
+                response = f"{OK} {PROTOCOL}\r\n\r\n".encode()
+                return response
+            elif self._method == "УДОЛИ":
+                path = Path(f"phonebook/{self._name}.txt")
+                if path.exists():
+                    path.unlink()
+                    response = f"{OK} {PROTOCOL}\r\n\r\n".encode()
+                else:
+                    response = f"{NOTFOUND} {PROTOCOL}\r\n\r\n".encode()
+                return response
+            else:
+                return f"{INCORRECT_REQUEST} {PROTOCOL}\r\n\r\n".encode()
+        else:
+            return f"{res_vragi}".encode()
+
+    def parse_response_check_server(self, res_vragi: str) -> bool:
+        """Функция для проверки МОЖНО или НИЛЬЗЯ"""
+        if res_vragi.startswith("МОЖНА"):
+            return True
+        else:
+            return False
 
 
 def run_server():
@@ -90,43 +108,11 @@ def run_server():
     server = socket.create_server(("localhost", 5000))
     server.listen(1)
     while True:
-        print("Главный цикл")
         conn, addr = server.accept()
+        print("Главный цикл")
         while True:
-            res = get_request(conn)
-            if res[0].startswith(INCORRECT_REQUEST):
-                send_response(conn, res.encode())
-            else:
-                permit, req_vragi = send_to_checking_server(res)
-                if permit:
-                    print(res)
-                    if "ЗОПИШИ" in res:
-                        with open(f"phonebook/{name}.txt", "w+") as file:
-                            file.write(phone_response)
-                        response = f"НОРМАЛДЫКС РКСОК/1.0\r\n\r\n".encode()
-                        send_response(conn, response)
-                    elif "УДОЛИ" in res[0]:
-                        path = Path(f"phonebook/{name}.txt")
-                        if path.exists():
-                            path.unlink()
-                            response = f"НОРМАЛДЫКС РКСОК/1.0\r\n\r\n".encode()
-                            send_response(conn, response)
-
-                        else:
-                            response = f"НИНАШОЛ РКСОК/1.0\r\n\r\n".encode()
-                            send_response(conn, response)
-
-                    elif "ОТДОВАЙ" in res[0]:
-                        path = Path(f"phonebook/{name}.txt")
-                        if path.exists():
-                            with path.open() as f:
-                                phone = "".join(f.readlines())
-                                print(phone)
-                        response = f"НОРМАЛДЫКС РКСОК/1.0\r\n{phone}\r\n\r\n".encode()
-                        send_response(conn, response)
-                else:
-                    response = req_vragi.encode()
-                    send_response(conn, response)
+            client = RKSOKPhoneBook(conn)
+            client.get_request()
             conn.close()
             break
     server.close()
