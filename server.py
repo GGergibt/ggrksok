6c8524301683f2ac9733ef830761ce7328fd3050
d1mnaz <dimonnaz@gmail.com>
2021-08-01 19:41:47 +0300

    перевел док стринги на английский язык

--- server.py
+++ server.py
@@ -1,18 +1,13 @@
-#!/home/www/code/ggrksok/venv/bin/python
 import socket
-import sys
 import threading
 import socketserver
 from utils import get_phonebook, write_phonebook, delete_phonebook
-# from loguru import logger
-
-
-# logger.add("debug.log", format="{time} {level} {message}", level="DEBUG")
+from typing import List
 
 
 PROTOCOL = "РКСОК/1.0"
 
-METOD = ("ОТДОВАЙ", "ЗОПИШИ", "УДОЛИ")
+METHOD = ("ОТДОВАЙ", "ЗОПИШИ", "УДОЛИ")
 OK = "НОРМАЛДЫКС"
 NOTFOUND = "НИНАШОЛ"
 NOT_APPROVED = "НИЛЬЗЯ"
@@ -26,34 +21,24 @@ class RKSOKPhoneBook:
         self._name, self._phone, self._method = None, None, None
         self.response = None
 
-
-    def raw_request(self, data):
-        msg = self.get_request(data)
-        # logger.debug(f"Запрос: {data}\nОТВЕТ: {msg.decode()}")
-        return msg
-
-    def get_request(self, msg):
-        """Функция для обработки запроса от клиента"""
+    def get_request(self, msg: str) -> str:
+        """Processing clients request"""
         request = [x for x in msg.split("\r\n") if len(x) > 0]
-        msg = self.compile_response(request)
+        if len(request) > 1:
+            self._phone = request[1::]
+            msg = self.compile_response(request[0])
+        else:
+            msg = self.compile_response(request[0])
         return msg
 
-    def compile_response(self, res):
-        """Функция собирает ответ для клиента"""
-        if len(res) > 1:
-            body_response = self.parse_body_request(res[0])
-            self._phone = res[1::]
-            raw_response = f"{body_response}\r\n{self._phone}"
-            self.response = self._raw_response(raw_response)
-        else:
-            try:
-                raw_response = self.parse_body_request(res[0])
-                self.response = self._raw_response(raw_response)
-            except IndexError as e:
-               pass
+    def compile_response(self, msg: str) -> str:
+        """Collects response for client"""
+        raw_response = self.parse_body_request(msg)
+        self.response = self._raw_response(raw_response)
         return self.response
 
     def _raw_response(self, response: str) -> str:
+        """Сheck that the request is correct"""
         if response.startswith(INCORRECT_REQUEST):
             raw_response = f"{INCORRECT_REQUEST} {PROTOCOL}\r\n\r\n".encode()
             return raw_response
@@ -61,12 +46,12 @@ class RKSOKPhoneBook:
         return response_client
 
     def parse_body_request(self, req: str) -> str:
-        """Функция для получения метода и имени пользователя"""
+        """Parsing request and methoв"""
         verbs = req.split()
         if verbs[-1] == PROTOCOL and self.get_method(verbs[0]):
             verbs.remove(PROTOCOL)
             verbs.pop(0)
-            if self.get_name(verbs[0]):
+            if self.get_name(verbs):
                 raw_response = f"{self._method} {self._name} {PROTOCOL}"
                 return raw_response
         else:
@@ -74,24 +59,24 @@ class RKSOKPhoneBook:
             return raw_response
 
     def get_method(self, method: str) -> str:
-        """Функция проверяет корректность метода РКСОК"""
-        if method in METOD:
+        """Get and checking method"""
+        if method in METHOD:
             self._method = method
             return self._method
 
-    def get_name(self, verbs) -> str:
-        """Функция проверяет имя пользователя"""
-        name = "".join(verbs)
+    def get_name(self, verbs: List[str]) -> str:
+        """Сollects the username into a single variable"""
+        name = " ".join(verbs)
         if self.checking_len_of_name(name):
             self._name = name
             return self._name
 
     def checking_len_of_name(self, name: str) -> bool:
-        """Функция проверки длины имени пользователя"""
+        """Return True if name is correct"""
         return len(name) <= 30 and len(name) != 0
 
     def send_to_checking_server(self, enquiry: str) -> str:
-        """Функция для запроса на сервер проверки"""
+        """Sends request to the serve checking"""
         method = "АМОЖНА? РКСОК/1.0"
         try:
             conn = socket.create_connection(("vragi-vezde.to.digital", 51624))
@@ -104,6 +89,7 @@ class RKSOKPhoneBook:
             print("Партия занята и не может ответить на твои глупые запросы")
 
     def response_processing(self, official_response: str) -> str:
+        """collets response after party verification"""
         if self.parse_response_check_server(official_response):
             if self.work_phonebook():
                 if self._method == "ОТДОВАЙ":
@@ -117,12 +103,12 @@ class RKSOKPhoneBook:
             return f"{official_response}".encode()
 
     def parse_response_check_server(self, official_response: str) -> bool:
-        """Функция для проверки МОЖНО или НИЛЬЗЯ"""
+        """Party verification of the request"""
         if official_response.startswith("МОЖНА"):
             return True
 
     def work_phonebook(self) -> bool:
-        """Функция для работы с телефонной книгой"""
+        """Working with utils phonebook"""
         if self._method == "ОТДОВАЙ":
             phone = get_phonebook(self._name)
             if phone:
@@ -135,11 +121,14 @@ class RKSOKPhoneBook:
             if delete_phonebook(self._name):
                 return True
 
+
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     pass
 
+
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
-    def recvall(self):
+    def recvall(self) -> str:
+        """Reading data from socket"""
         BUFF_SIZE = 2048
         data = ""
         while True:
@@ -151,15 +140,18 @@ class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
         return data
 
     def handle(self):
+        """Function getting request from user,
+        sends to the RKSOK class and returns the response to the user
+        """
         data = self.recvall()
         cur_thread = threading.current_thread()
         client = RKSOKPhoneBook()
-        response = client.raw_request(data)
-        self.request.sendall(response)
+        official_response = client.get_request(data)
+        self.request.sendall(official_response)
 
 
-
-if __name__ == "__main__":
+def run_server():
+    """Start the server RKSOK"""
     HOST, PORT = "", 50007
     server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
     with server:
@@ -170,3 +162,7 @@ if __name__ == "__main__":
         print("Server loop running in thread:", server_thread.name)
 
         server.serve_forever()
+
+
+if __name__ == "__main__":
+    run_server()
