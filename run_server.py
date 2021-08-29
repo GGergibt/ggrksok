e2229a3f5d8a673a98725ec98e2117852d3d65b8
GGergibt <gosha2007naz@gmail.com>
2021-08-29 20:06:14 +0300

    ref run_server

--- run_server.py
+++ run_server.py
@@ -2,38 +2,28 @@ import asyncio
 from RKSOKPhonebook import RKSOKPhoneBook
 
 
-async def recvall(reader):
+async def recvall(reader) -> str:
     """Reading data from socket"""
-    data = b""
+    data = b''
     while True:
-        part = await reader.read(100)
+        part = await reader.read(2048)
         data += part
         end = b"\r\n\r\n"
-        if part.endswith(end):
+        if data.endswith(end) or part == b'':
             break
-    return data
-
+    return data.decode()
 
 async def handle_echo(reader, writer):
-    # data = await reader.read(100)
     data = await recvall(reader)
-    message = data.decode()
     client = RKSOKPhoneBook()
-    official_response = client.get_request(message)
-
+    official_response = client.get_request(data)
     writer.write(official_response)
     await writer.drain()
-
-    print("Close the connection")
     writer.close()
 
 
 async def main():
-    server = await asyncio.start_server(handle_echo, "127.0.0.1", 50007)
-
-    addr = server.sockets[0].getsockname()
-    print(f"Serving on {addr}")
-
+    server = await asyncio.start_server(handle_echo, "", 50007)
     async with server:
         await server.serve_forever()
 
